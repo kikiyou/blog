2c16820fb9cc00bd2aa13d5ac7f17ecad9b15b55
monkey-master <admin@rhel.cc>
2017-12-26 20:34:27 +0800

    添加blackbox_exporter

---
+++ source/_posts/Prometheus/blackbox_exporter/不错的代码思想/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"probers"
+)
+
+var addr = flag.String("web.listen-address", ":8080", "The address to listen on for HTTP requests.")
+
+type Config struct {
+	Modules map[string]Module `yaml:"modules"`
+}
+
+type Module struct {
+	Prober  string        `yaml:"prober"`
+	Timeout time.Duration `yaml:"timeout"`
+	Config  interface{}   `yaml:"config"`
+}
+
+func probeHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	target := params.Get("target")
+	module := params.Get("module")
+	if target == "" {
+		http.Error(w, "Target parameter is missing", 400)
+		return
+	}
+	if module == "" {
+		module = "http2xx"
+	}
+	start := time.Now()
+	success := probers.Probers["http"](target, w)
+	fmt.Fprintf(w, "probe_duration_seconds %f\n", float64(time.Now().Sub(start))/1e9)
+	if success {
+		fmt.Fprintf(w, "probe_success %d\n", 1)
+	} else {
+		fmt.Fprintf(w, "probe_success %d\n", 0)
+	}
+}
+
+func main() {
+	flag.Parse()
+	http.Handle("/metrics", prometheus.Handler())
+	http.HandleFunc("/probe", probeHandler)
+	http.ListenAndServe(*addr, nil)
+}
